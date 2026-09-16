1b6955f571da909daa2892f654ecf993d3d62243
Shivakumar <mr.shivakumar@kaplan.edu>
2026-09-16 13:55:05 +0530

    Login api renamed

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -12,7 +12,7 @@ const {
 
 // --- Public Routes ---
 router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/logins', loginUser);
 
 // --- Protected Route ---
 // ✅ CORRECTED: Use 'authMiddleware' directly, without calling it with ()
